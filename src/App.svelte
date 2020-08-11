9418fd56e253fa3ad708f49cc3315813332693cf
Brhane <godseeshears@gmail.com>
2020-08-11 12:08:07 +0200

    WashingHand: some user interface is done

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,25 @@
+<script>
+import Timer from "./Timer.svelte";
+import Howto from "./Howto.svelte";
+</script>
+
+<style>
+h1, h3 {
+    text-align: center;
+}
+a {
+    padding-right: 10px;
+}
+</style>
 <h1>
-	Test
+	HandWashing App
 </h1>
+
+<Timer />
+
+<Howto/>
+
+<h3>
+<a href="https://www.who.int/gpsc/clean_hands_protection/en/">Picture source </a>
+<a href="https://freesound.org/people/metrostock99/sounds/345086/"> Sound source</a>
+</h3>
---
+++ src/HowTo.svelte
@@ -0,0 +1,13 @@
+<script>
+    let src = 'handwashing.gif';
+</script>
+<style>
+    img {
+        max-width: 100%;
+    }
+</style>
+<div bp="grid">
+    <img
+    bp="offset-5@md 4@md 12@sm"
+    {src} alt="How to wash your hands."/>
+</div>
